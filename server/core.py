9fddb23abe24a2f9521b54f4af1697220eef1c87
Klutton <113648660+Klutton@users.noreply.github.com>
2023-01-05 22:04:53 +0800

    supporting redis

--- server/core.py
+++ server/core.py
@@ -1,23 +1,30 @@
 import os
 import json
 import uuid
-
+import redis
 from flask import Flask, request, make_response, session, redirect
 from flask_login import LoginManager
 
-pending_invite_code = str(uuid.uuid4()).replace('-', '')
-print(pending_invite_code)
 invite_code_list: list = []
 
+# redis config
+pool = redis.ConnectionPool(host='127.0.0.1')
+_redis = redis.Redis(connection_pool=pool)
+
+# flask config
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 login_manager = LoginManager()
 
 
-def new_code():
-    global pending_invite_code
-    pending_invite_code = pending_invite_code = str(uuid.uuid4()).replace('-', '')
-    return
+def new_code(seconds: int = 60 * 60 * 72):  # 默认有效时间为72小时
+    pending_invite_code = str(uuid.uuid4()).replace('-', '')
+    _redis.set(name=pending_invite_code, value='value_is_meaningless', ex=seconds)
+    return pending_invite_code
+
+
+def identify_code(code: str):
+    return True if _redis.exists(code) else False
 
 
 """@login_manager.user_loader()
@@ -32,7 +39,8 @@ def identify():
     resp = make_response(redirect(request.path))
     if not request.cookies or session:
         session['id'] = str(uuid.uuid4())
-        resp.set_cookie("name", "python")
+        if not session['user']:
+            session['user'] = None
         return resp
     else:
         pass
@@ -41,19 +49,25 @@ def identify():
 @app.route("/n2n_login", methods=['POST'])
 def login():
 
-    return f"{request.form['username'], request.form['password']}"
 
 
-@app.route("/n2n_register", methods=['POST'])
+@app.route("/n2n_register", methods=['GET', 'POST'])
 def register():
-    if not request.form['code'] in app.secret_key:
-        return json.dump({'status': 'wrong_code'})
+    if request.method == 'POST':
+        if identify_code(request.form['code']):
+            create
+        else:
+            return "invalid invite_code!"
 
 
 @app.route("/n2n_getcode", methods=["POST"])
 def get_code():
-    # if request.form['token'] ==
-    return 'not yet'
+    if session['username'] is None:
+        return 'Please Login'
+    elif session['auth'] == 'admin':
+        return new_code()
+    else:
+        return 'flag{G1ve_u_f3ke_fL4g}'
 
 
 @app.route("/test")
