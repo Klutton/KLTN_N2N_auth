8d8db760ac8d81b081b66ed5478bd8a8b5e056d1
Klutton <113648660+Klutton@users.noreply.github.com>
2023-01-05 16:22:12 +0800

    验证session

--- server/core.py
+++ server/core.py
@@ -1,7 +1,9 @@
 import os
 import json
 import uuid
-from flask import Flask, request
+
+from flask import Flask, request, make_response, session, redirect
+from flask_login import LoginManager
 
 pending_invite_code = str(uuid.uuid4()).replace('-', '')
 print(pending_invite_code)
@@ -9,6 +11,7 @@ invite_code_list: list = []
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
+login_manager = LoginManager()
 
 
 def new_code():
@@ -17,21 +20,47 @@ def new_code():
     return
 
 
+"""@login_manager.user_loader()
+def load_user(user_id):
+    if user_id == 'asd':
+        return User.get(user_id)
+"""
+
+
 @app.before_request
 def identify():
-
+    resp = make_response(redirect(request.path))
+    if not request.cookies or session:
+        session['id'] = str(uuid.uuid4())
+        resp.set_cookie("name", "python")
+        return resp
+    else:
+        pass
 
 
 @app.route("/n2n_login", methods=['POST'])
 def login():
+
     return f"{request.form['username'], request.form['password']}"
 
 
 @app.route("/n2n_register", methods=['POST'])
 def register():
-    if request.form['code'] != app.secret_key:
+    if not request.form['code'] in app.secret_key:
         return json.dump({'status': 'wrong_code'})
 
 
+@app.route("/n2n_getcode", methods=["POST"])
+def get_code():
+    # if request.form['token'] ==
+    return 'not yet'
+
+
+@app.route("/test")
+def test():
+    return str(session.get('id'))
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+    # login_manager.init_app(app)
